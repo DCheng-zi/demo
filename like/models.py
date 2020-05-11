456a4e2d04bfb3f490285510740c6d6e882c9a54
程子恺 <cheng_zi0219@163.com>
2020-05-11 17:58:17 +0800

    添加了like模型‘ git commit -m 添加了like模型‘

--- like/models.py
+++ like/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Like(models.Models):
+    uid = models.IntegerField()
+    wid = models.IntegerField()
+    created = models.DateTimeField()
